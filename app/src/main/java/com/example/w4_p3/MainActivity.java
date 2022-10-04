e3fe0d7237e8b7c0559528b32302946ccaf115a1
Pratham Shroff <pshroff@bu.edu>
2022-10-04 19:24:56 -0400

    Button & WebView somewhat working

--- app/src/main/java/com/example/w4_p3/MainActivity.java
+++ app/src/main/java/com/example/w4_p3/MainActivity.java
@@ -3,12 +3,28 @@ package com.example.w4_p3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button button1;
+    private WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        WebView webView = (WebView) findViewById(R.id.webView);
+
+        button1 = (Button) findViewById(R.id.button1) ;
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                webView.loadUrl("http://webb.nasa.gov/");
+            }
+        });
     }
 }
