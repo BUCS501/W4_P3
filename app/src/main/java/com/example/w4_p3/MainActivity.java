f107f070985f37f272a60247edd513996c9670e0
Pratham Shroff <pshroff@bu.edu>
2022-10-04 22:40:14 -0400

    Shake Feature Implemented, SeekBar Tested

--- app/src/main/java/com/example/w4_p3/MainActivity.java
+++ app/src/main/java/com/example/w4_p3/MainActivity.java
@@ -35,7 +35,8 @@ public class MainActivity extends AppCompatActivity {
         String URL;
         if (option.equals("X")) URL = "https://www.ecosia.org/";
         else if (option.equals("Y")) URL = "https://www.dogpile.com/";
-        else if (option.equals("Z")) URL = "https://webb.nasa.gov/ ";
+        else if (option.equals("Z")) URL = "https://webb.nasa.gov/";
+        else if (option.equals("DIZZ ME!")) URL = "https://jumpingjaxfitness.files.wordpress.com/2010/07/dizziness.jpg ";
 
         // random 404 error msg page - FOR FUN
         else URL = "https://github.com/karenLee57/karenLee57.github.io-hangman";
@@ -64,11 +65,17 @@ public class MainActivity extends AppCompatActivity {
             if (changedPos[2] > largestPosChange) { largestPosChange = changedPos[2]; largestPosDir = "Z"; }
 
             if (largestPosChange > seekBarProgress && !prevPosDir.equals(largestPosDir)) {
-                Toast.makeText(getApplicationContext(), ("Movement on the " + largestPosDir + " axis by " + String.valueOf(largestPosChange)), Toast.LENGTH_SHORT).show();
-                Log.i(largestPosDir,("Change in " + largestPosDir + " axis"));
+                String toastText = ("Movement on the " + largestPosDir + " axis");
+
+                // To test how much the change was & if seekBar works!
+                // toastText = ("Movement on the " + largestPosDir + " axis by " + String.valueOf(largestPosChange));
+
+                Toast.makeText(getApplicationContext(), toastText, Toast.LENGTH_SHORT).show();
+                Log.i(largestPosDir, ("Change in " + largestPosDir + " axis"));
                 changeDisplayURL(largestPosDir);
                 prevPosDir = largestPosDir;
             }
+            if (largestPosChange > 20) changeDisplayURL("DIZZ ME!");
         }
 
         @Override
