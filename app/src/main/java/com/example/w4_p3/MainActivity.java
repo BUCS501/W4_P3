8a4fdf5f263011b992cf58d06e366660001de3cd
Pratham Shroff <pshroff@bu.edu>
2022-10-04 20:39:49 -0400

    Added textview and seekbar, planned sensorManager
    Everything works great! Layout for Nexus still to be fixed.

--- app/src/main/java/com/example/w4_p3/MainActivity.java
+++ app/src/main/java/com/example/w4_p3/MainActivity.java
@@ -1,25 +1,81 @@
 package com.example.w4_p3;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.pm.ActivityInfo;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.view.View;
 import android.webkit.WebView;
 import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button button1;
+    private Button button;
     private WebView webView;
+    private TextView textView;
+    private SeekBar seekBar;
+    private int seekBarProgress;
+    public String largestChangeDir;
+
+    private SensorManager sensorManager;
+    private Sensor accelerometer;
+
+    private SensorEventListener sensorEventListener = new SensorEventListener() {
+        @Override
+        public void onSensorChanged(SensorEvent sensorEvent) {
+
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int i) {
+
+        }
+    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        WebView webView = (WebView) findViewById(R.id.webView);
-        button1 = (Button) findViewById(R.id.button1) ;
+        // FIX ORIENTATION - PORTRAIT
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
-        button1.setOnClickListener(new View.OnClickListener() {
+        textView = (TextView) findViewById(R.id.textView) ;
+
+        sensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        seekBar = (SeekBar) findViewById(R.id.seekBar);
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                textView.setText("Significant Change = " + String.valueOf(i));
+                seekBarProgress = i;
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        WebView webView = (WebView) findViewById(R.id.webView);
+        button = (Button) findViewById(R.id.button) ;
+
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 webView.loadUrl("https://www.ecosia.org/");
@@ -27,3 +83,4 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 }
+
